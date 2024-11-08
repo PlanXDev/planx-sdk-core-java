efba5f1449cad492c80c61b713e682ddf36ddae7
Todd <qq199407231031>
2024-11-08 09:02:35 +0000

    update

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # planx-sdk-core-java
 Here is the java version of PlanXSDK.
+
+
+## First step:
+> * Go to the **[PlanX CP](https://cp.planx.io)** to register.
+> * Get **AppId** and **SecretKey**.
+## Second step:
+> * Install library into your Maven's local repository by running `mvn install`
+> * Add the following Maven dependency to your project's `pom.xml`:
+```
+    <dependency>
+      <groupId>io.planckx.api</groupId>
+      <artifactId>planx-sdk-core-java</artifactId>
+      <version>0.1.0</version>
+    </dependency>
+```
+## Example Usage
+> * This example get balance information for all token accounts:
+```java
+    String baseUrl = "http://cp-api.planx.io";
+    String appId = "Go to the PlanX CP website to get";
+    String secretKey = "Go to the PlanX CP website to get";
+    PlanXClientFactory planXClientFactory = PlanXClientFactory.newInstance(baseUrl,appId, secretKey);
+    List<ResponseBalancesBase> balancesBase = planXClientFactory.newAccountClient().getAccountBalancesBase();
+    
+    // Process your bussiness ...
+```
+
+## Other
+> * Supports JDK 1.8+
--- src/main/java/io/planx/api/Test.java
+++ src/main/java/io/planx/api/Test.java
@@ -1,6 +1,7 @@
 package io.planx.api;
 
 import io.planx.api.common.PlanXClientFactory;
+import io.planx.api.model.response.ResponseBalancesBase;
 import io.planx.api.model.response.ResponseGiftSourceDetail;
 
 import java.util.List;
@@ -11,11 +12,12 @@ import java.util.List;
  **/
 public class Test {
     public static void main(String[] args){
-        PlanXClientFactory planXClientFactory = PlanXClientFactory.newInstance("http://192.168.1.200:8886",
-                "cg7Zqpb4", "842c2d804f5b5e9bba7a35436d9585b26bca4c69");
-        List<ResponseGiftSourceDetail> sourceInfoBatch = planXClientFactory.newGiftClient().
-                getGiftSourceInfoBatch(new String[]{"1E44BA0A-6D95-450B-9D0D-9F934FC5EFCA",
-                        "215FF713-D8C5-44DD-9597-9CCD722BE39E","498DDBA1-E4AD-45CD-A9A1-27A11DDC201B"});
-        System.out.println(sourceInfoBatch.toString());
+        String baseUrl = "http://cp-api.planx.io";
+        String appId = "Go to the PlanX CP website to get";
+        String secretKey = "Go to the PlanX CP website to get";
+        PlanXClientFactory planXClientFactory = PlanXClientFactory.newInstance(baseUrl,appId, secretKey);
+        List<ResponseBalancesBase> balancesBase = planXClientFactory.newAccountClient().getAccountBalancesBase();
+
+        // Process your bussiness ...`.`
     }
 }
